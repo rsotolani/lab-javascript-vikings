a099507c0395050fe4a5337b1f9620b1e871bab5
Rodrigo Sotolani <rsotolani@gmail.com>
2022-10-24 16:41:09 -0300

    ate iteracao 4 completo

--- src/viking.js
+++ src/viking.js
@@ -3,7 +3,7 @@ class Soldier {
     constructor(health, strength) {
         this.health = health;
         this.strength = strength;
-        console.log("Classe Soldier");
+        //console.log("Classe Soldier");
     }
 
     attack() {
@@ -42,12 +42,12 @@ class Viking extends Soldier {
 class Saxon extends Soldier {
     constructor(health, strength) {
         super(health, strength);
-        console.log("Classe Saxon", this);
+        //console.log("Classe Saxon", this);
     }
 
     receiveDamage(damage) {
         super.receiveDamage(damage);
-        console.log(this.health);
+        //console.log(this.health);
         if (this.health > 0) {
             return `A Saxon has received ${damage} points of damage`;
         } else {
@@ -55,18 +55,98 @@ class Saxon extends Soldier {
         }
         
     }
-
-    battleCry() {
-        return `Odin Owns You All!`;
-    }
 }
 
 // War
 class War {
-    
+
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+
+    addViking(viking) {        
+        this.vikingArmy.push(viking);
+    }
+
+    addSaxon(saxon) {
+        this.saxonArmy.push(saxon);
+    }
+
+    vikingAttack() {
+        /* testes de randomico...
+
+        let r = Math.random();
+        let s = Math.random();
+        let t = Math.random();
+        console.log(r,
+                    this.vikingArmy.length,
+                    r * this.vikingArmy.length,
+                    Math.floor(r * this.vikingArmy.length));
+        console.log(s,
+                    this.vikingArmy.length,
+                    s * this.vikingArmy.length,
+                    Math.floor(s * this.vikingArmy.length));
+        console.log(t,
+                    this.vikingArmy.length,
+                    t * this.vikingArmy.length,
+                    Math.floor(t * this.vikingArmy.length));
+        */
+        
+
+        let vikingRandom = Math.floor(this.vikingArmy.length * Math.random());
+        let viking = this.vikingArmy[vikingRandom];
+
+        let saxonRandom = Math.floor(this.saxonArmy.length * Math.random());
+        let saxon = this.saxonArmy[saxonRandom];
+
+        let attack = saxon.receiveDamage(viking.strength);
+
+        if (saxon.health <= 0) this.saxonArmy.splice(saxonRandom, 1);
+
+        return attack;
+    }
+
+    saxonAttack() {
+
+        let vikingRandom = Math.floor(this.vikingArmy.length * Math.random());
+        let viking = this.vikingArmy[vikingRandom];
+
+        let saxonRandom = Math.floor(this.saxonArmy.length * Math.random());
+        let saxon = this.saxonArmy[saxonRandom];
+
+        let attack = viking.receiveDamage(saxon.strength);
+
+        if (viking.health <= 0) this.vikingArmy.splice(vikingRandom, 1);
+
+        return attack;
+
+    }
+
+    showStatus() {
+
+    }
 
 }
 
-const soldier = new Soldier(100,10);
-const viking = new Viking("Rodrigo",100,20);
-console.log(viking.receiveDamage(20));
+const war = new War();
+
+war.addViking(new Viking("R",100,50));
+//war.addViking(new Viking("S",100,50));
+//war.addViking(new Viking("T",100,50));
+//war.addViking(new Viking("U",100,50));
+//war.addViking(new Viking("V",100,50));
+//console.log(war.vikingArmy);
+
+war.addSaxon(new Saxon(50,10));
+war.addSaxon(new Saxon(50,10));
+//war.addSaxon(new Saxon(50,10));
+//war.addSaxon(new Saxon(50,10));
+//war.addSaxon(new Saxon(50,10));
+//console.log(war.saxonArmy);
+
+console.log(war.vikingAttack());
+console.log(war.saxonAttack());
+
+
+
